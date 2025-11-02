69b221fa1350be7bc68155f49d8ae249b8881a96
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2025-11-02 12:32:00 +0530

    Basic struction of java file

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello Java, welcome in java world and its you first Java program");
+public class Main{
+    // --------Classes----------------
+    // public :- access this Classes anywhere in the application
+    // Main :- its class name, should be in the pascalCase.
+    // Note :- Main class is the entry point of the application
+    public static void main(String[] args){
+        // --------Function(methods)-----------------
+        // public :- access this function(method) from anywhere in the application
+        // static :- we can directly use this function(method), no need to create object from respective class.
+        // void :- its indicates that function(method) does not return anythings(values).
+        // main :- its function(method) name, should be in the camelCase.
 
-
+        System.out.println("Described Java file's basic structure");
+        // To view/print the values
     }
 }
+
