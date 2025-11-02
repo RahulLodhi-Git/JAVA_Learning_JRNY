9d7d13b39caa3ac4dae4908fa2dba0ca9ad1412d
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2025-11-02 19:23:03 +0530

    Tuts04 | Primitive dataTypes and literals

---
+++ src/Tut04_literals.java
@@ -0,0 +1,23 @@
+public class Tut04_literals {
+    public static void main(String[] args) {
+        // Literals :- When we assigned any constant into the variable is called literals. For eg
+        byte age =55;
+        int age2=97;
+        short age3=99;
+        long higherNumberValue=98888888899L;
+        float floatVar=0.9f;
+        double doubleVar=0.8; // No need explicitly add the 'd' or 'D' at end of double variable, as it default considering double number.
+        boolean booleanVar= false;
+        char charVar='B';
+        String StringVar="Rahul Lodhi";
+
+
+
+
+
+
+        System.out.println(charVar + StringVar);
+
+
+    }
+}
