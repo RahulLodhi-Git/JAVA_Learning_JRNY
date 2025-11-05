d4a9d022066dd0d915bf374b099cc8b8d8202b59
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2025-11-05 19:15:49 +0530

    Tuts07 | Exercise_02 | 2nd Method check given input is integer or not

--- src/Tut07_Exercise02.java
+++ src/Tut07_Exercise02.java
@@ -5,15 +5,23 @@ public class Tut07_Exercise02 {
     public static void main(String[] args){
         Scanner userInput=new Scanner(System.in);
         System.out.println("Enter a number to check if is Integer or not... ?");
-        String inputString=userInput.nextLine();
-      try{
-          Integer.parseInt(inputString);
-            System.out.println("Given Input '"+inputString+ "' is Integer.");
-      }catch(NumberFormatException e){
-          System.out.println("Given number '"+inputString+ "' is not Integer, Reason is "+e);
+
+//     -------1st Method---------
+//        String inputString=userInput.nextLine();
+//      try{
+//          Integer.parseInt(inputString);
+//            System.out.println("Given Input '"+inputString+ "' is Integer.");
+//      }catch(NumberFormatException e){
+//          System.out.println("Given number '"+inputString+ "' is not Integer, Reason is "+e);
+//        }
+
+//      -------2nd Method---------
+        if(userInput.hasNextInt()){
+            System.out.println("Given Input '"+userInput.nextInt()+ "' is Integer.");
+        }else{
+            System.out.println("Given number '"+userInput.nextLine()+ "' is not Integer.");
         }
+
         userInput.close();
-
-
     }
 }
