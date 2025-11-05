0f70bef16710d11f732ebfcb6bd28622b5294540
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2025-11-05 20:04:04 +0530

    Tuts09 & Tuts10 | Operator Precedence & Associativity

---
+++ src/Tut09_OperatorPrecedenceAndAssociativity.java
@@ -0,0 +1,5 @@
+/* In java, All operator have own precedence and associativity over another. For e.g. brackets have the highest precedence then (*,&)so its evaluate first. Check oracle documents.
+
+ */
+public class Tut09_OperatorPrecedenceAndAssociativity {
+}
---
+++ src/Tut10_DataTypeOfExpression.java
@@ -0,0 +1,7 @@
+
+/* In java, DataTypeOfExpression are means, when we evaluate a expression where different type of dataType are used, what will the dataType of result. For e.g.
+Int+Sting=int, byte+int= int, etc.
+
+ */
+public class Tut10_DataTypeOfExpression {
+}
